b8a6e69156807e1d175387c2947ad0b1e4478668
leafgg <jobs.lugui@gmail.com>
2024-05-21 17:22:14 -0300

    feat: rxjs zip example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
+import { SchoolData, SchoolService } from './services/school.service';
+import { Observable, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,36 @@ import { TodoCardComponent } from './components/todo-card/todo-card.component';
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit{
+  public title = 'todo-list';
+  public students: Array<SchoolData> = [];
+  public teachers: Array<SchoolData> = [];
+  private zipSchoolResponses$ = zip(
+    this.getStudentsDatas(),
+    this.getTeachersDatas()
+  );
+
+  constructor(private schoolService: SchoolService){}
+
+  public ngOnInit(): void {
+    this.getSchoolDatas();
+
+  }
+  public getSchoolDatas(): void {
+    this.zipSchoolResponses$.subscribe({
+      next: (response) => {
+        console.log('students', response[0]);
+        console.log('teachers', response[1]);
+      },
+    })
+  }
+
+  private getStudentsDatas(): Observable<Array <SchoolData>>{
+    return this.schoolService.getStudents();
+  }
+
+  private getTeachersDatas(): Observable<Array <SchoolData>>{
+    return this.schoolService.getTeachers();
+  }
+
 }

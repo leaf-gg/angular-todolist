2cf68659039391f3c6781cec9304229caa1305ec
leafgg <jobs.lugui@gmail.com>
2024-05-21 16:45:12 -0300

    feat: rxjs of operator example

---
+++ src/app/services/school.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface SchoolData {
+  name: string;
+  id: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SchoolService {
+  private students: Array<SchoolData> = [
+    { name: 'Julia', id: '1' },
+    { name: 'Marcus', id: '2' },
+    { name: 'Isabel', id: '3' },
+  ];
+
+  private teachers: Array<SchoolData> = [
+    {
+      name: 'Magda',
+      id: '1',
+    },
+    {
+      name: 'André',
+      id: '2',
+    },
+    {
+      name: 'Val',
+      id: '3',
+    },
+  ];
+  public getStudents(): Observable<Array<SchoolData>> {
+    return of(this.students);
+  }
+
+  public getTeachers(): Observable<Array<SchoolData>> {
+    return of(this.teachers);
+  }
+}

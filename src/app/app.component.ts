3fdecc0eee4b4fab05eaccc5e57ef67f733bfd49
leafgg <jobs.lugui@gmail.com>
2024-05-22 09:59:51 -0300

    feat: filter example rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoCardComponent } from './components/todo-card/todo-card.component';
 import { SchoolData, SchoolService } from './services/school.service';
-import { Observable, from, map, of, zip } from 'rxjs';
+import { Observable, filter, from, map, of, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, HeaderComponent, TodoCardComponent],
   templateUrl: './app.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   public title = 'todo-list';
   public students: Array<SchoolData> = [];
   public teachers: Array<SchoolData> = [];
@@ -22,33 +22,36 @@ export class AppComponent implements OnInit{
   );
   private ages = of(20, 30, 50, 55, 25, 17);
   private peopleDatas = from([
-    {name: 'Luiz', age: 28, profession: 'Developer' },
-    {name: 'Sakura', age: 18, profession: 'Student' },
-    {name: 'Takezo', age: 58, profession: 'Lone Samurai' },
+    { name: 'Luiz', age: 28, profession: 'Developer' },
+    { name: 'Sakura', age: 18, profession: 'Student' },
+    { name: 'Takezo', age: 58, profession: 'Lone Samurai' },
+    { name: 'Valdir', age: 35, profession: 'Mechanic' },
+    { name: 'Carla', age: 22, profession: 'Barber' },
   ]);
 
-  constructor(private schoolService: SchoolService){}
+  constructor(private schoolService: SchoolService) {}
 
   public ngOnInit(): void {
     // this.getSchoolDatas();
     //this.getMultipliedAges();
-    this.getPeopleProfession();
+    this.getPeopleNames();
   }
 
-  public getPeopleProfession(): void {
-      this.peopleDatas.pipe(
-        map((person) => person.profession))
-        .subscribe({
-          next: (response) => console.log(response)
-        })
-  }
-
-  public getMultipliedAges():void {
-    this.ages.pipe(
-      map((age) => age * age))
+  public getPeopleNames(): void {
+    this.peopleDatas
+      .pipe(
+        filter((person) => person.age > 20),
+        map((person) => person.name)
+      )
       .subscribe({
-        next: (response) => console.log('age', response)
-      })
+        next: (response) => console.log(response),
+      });
+  }
+
+  public getMultipliedAges(): void {
+    this.ages.pipe(map((age) => age * age)).subscribe({
+      next: (response) => console.log('age', response),
+    });
   }
 
   public getSchoolDatas(): void {
@@ -57,15 +60,14 @@ export class AppComponent implements OnInit{
         console.log('students', response[0]);
         console.log('teachers', response[1]);
       },
-    })
+    });
   }
 
-  private getStudentsDatas(): Observable<Array <SchoolData>>{
+  private getStudentsDatas(): Observable<Array<SchoolData>> {
     return this.schoolService.getStudents();
   }
 
-  private getTeachersDatas(): Observable<Array <SchoolData>>{
+  private getTeachersDatas(): Observable<Array<SchoolData>> {
     return this.schoolService.getTeachers();
   }
-
 }
